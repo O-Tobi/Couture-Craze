5c37152e063cac34a37585946601318a485b6578
Oluwatobiloba Oladunjoye <dunjoyebiz2020@gmail.com>
2024-02-26 16:03:05 +0100

    form input and button component

--- src/components/sign-up/sign-up-form.js
+++ src/components/sign-up/sign-up-form.js
@@ -4,6 +4,10 @@ import {
     createUserDocumentFromAuth
      } from '../../utils/firebase/firebase';
 
+import FormInput from '../form-input/form-input';
+import './sign-up-form.scss';
+import Button from '../button/button';
+
 const defaultFormFields = {
     displayName: '',
     email: '',
@@ -57,11 +61,13 @@ const SignUpForm = () => {
     };
 
     return (
-        <div>
-            <h1>Sign up with your email and password</h1>
+        <div className='sign-up-container'>
+            <h2>Don't have an account?</h2>
+            <span>Sign up with your email and password</span>
             <form onSubmit={handleSubmit}>
-                <label>Display Name</label>
-                <input 
+                
+                <FormInput 
+                label= "Display Name"
                 type="text" 
                 required 
                 onChange={handleChange} 
@@ -69,8 +75,9 @@ const SignUpForm = () => {
                 value={displayName}
                 />
 
-                <label>Email</label>
-                <input 
+                
+                <FormInput
+                label ='Email' 
                 type="email" 
                 required 
                 onChange={handleChange} 
@@ -78,8 +85,9 @@ const SignUpForm = () => {
                 value={email}
                 />
 
-                <label>Password</label>
-                <input 
+                
+                <FormInput
+                label = 'Password' 
                 type="password" 
                 required 
                 onChange={handleChange}
@@ -87,14 +95,15 @@ const SignUpForm = () => {
                 value={password}
                  />
 
-                <label>Confirm Password</label>
-                <input 
+              
+                <FormInput 
+                label = 'Confirm Password'
                 type="password" 
                 required 
                 onChange={handleChange} 
                 name='confirmPassword'
                 value={confirmPassword}/>
-                <button type="submit">Sign Up</button>
+                <Button buttonType='google' type="submit">Sign Up</Button>
             </form>
         </div>
     )
